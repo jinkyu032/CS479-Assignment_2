lesson: torch.repeat can result in gradient different with auto broadcasting. And auto casting is may be correct way when doing arithmetic

torch_nerf/src/network/nerf.py
@@ -39,7 +39,8 @@ class NeRF(nn.Module):
         MLP_firstinput_to_beforeskip = []
         MLP_firstinput_to_beforeskip.append(nn.Linear(pos_dim, feat_dim))
         MLP_firstinput_to_beforeskip.append(nn.ReLU())
-        for _ in range(3):
+        #for _ in range(3):
+        for _ in range(4):
             MLP_firstinput_to_beforeskip.append(nn.Linear(feat_dim, feat_dim))
             MLP_firstinput_to_beforeskip.append(nn.ReLU())
         self.MLP_firstinput_to_beforeskip = nn.Sequential(*MLP_firstinput_to_beforeskip)
@@ -47,7 +48,8 @@ class NeRF(nn.Module):
         MLP_skip_to_beforeviewdir = []
         MLP_skip_to_beforeviewdir.append(nn.Linear(feat_dim + pos_dim, feat_dim))
         MLP_skip_to_beforeviewdir.append(nn.ReLU())
-        for _ in range(3):
+        #for _ in range(3):
+        for _ in range(2):
             MLP_skip_to_beforeviewdir.append(nn.Linear(feat_dim, feat_dim))
             MLP_skip_to_beforeviewdir.append(nn.ReLU())
         self.MLP_skip_to_beforeviewdir = nn.Sequential(*MLP_skip_to_beforeviewdir)
@@ -67,7 +69,7 @@ class NeRF(nn.Module):
         self,
         pos: Float[torch.Tensor, "num_sample pos_dim"],
         view_dir: Float[torch.Tensor, "num_sample view_dir_dim"],
-    ) -> Tuple[Float[torch.Tensor, "num_sample 1"], Float[torch.Tensor, "num_sample 3"]]:
+    ) -> Tuple[torch.Tensor, torch.Tensor]:#[Float[torch.Tensor, "num_sample 1"], Float[torch.Tensor, "num_sample 3"]]:
         """
         Predicts color and density.
 
@@ -85,8 +87,121 @@ class NeRF(nn.Module):
         feat = self.MLP_firstinput_to_beforeskip(pos)
         feat = self.MLP_skip_to_beforeviewdir(torch.cat([pos, feat], dim=-1))
         sigma_feat = self.view_layer(feat)
-        sigma = torch.relu(sigma_feat[..., :1])
+        #sigma = torch.relu(sigma_feat[..., :1])
+        sigma = torch.relu(sigma_feat[..., 0])
         feat = sigma_feat[..., 1:]
         feat = self.relu(self.additional_layer(torch.cat([feat, view_dir], dim=-1)))
         rgb = self.sigmoid(self.final_layer(feat))
         return sigma, rgb
+
+# class NeRF(nn.Module):
+#     """
+#     A multi-layer perceptron (MLP) used for learning neural radiance fields.
+
+#     For architecture details, please refer to 'NeRF: Representing Scenes as
+#     Neural Radiance Fields for View Synthesis (ECCV 2020, Best paper honorable mention)'.
+
+#     Attributes:
+#         pos_dim (int): Dimensionality of coordinate vectors of sample points.
+#         view_dir_dim (int): Dimensionality of view direction vectors.
+#         feat_dim (int): Dimensionality of feature vector within forward propagation.
+#     """
+
+#     def __init__(
+#         self,
+#         pos_dim: int,
+#         view_dir_dim: int,
+#         feat_dim: int = 256,
+#     ):
+#         """
+#         Constructor of class 'NeRF'.
+
+#         Args:
+#             pos_dim (int): Dimensionality of coordinate vectors of sample points.
+#             view_dir_dim (int): Dimensionality of view direction vectors.
+#             feat_dim (int): Dimensionality of feature vector within forward propagation.
+#                 Set to 256 by default following the paper.
+#         """
+#         super().__init__()
+
+#         rgb_dim = 3
+#         density_dim = 1
+
+#         self._pos_dim = pos_dim
+#         self._view_dir_dim = view_dir_dim
+#         self._feat_dim = feat_dim
+
+#         # fully-connected layers
+#         self.fc_in = nn.Linear(self._pos_dim, self._feat_dim)
+#         self.fc_1 = nn.Linear(self._feat_dim, self._feat_dim)
+#         self.fc_2 = nn.Linear(self._feat_dim, self._feat_dim)
+#         self.fc_3 = nn.Linear(self._feat_dim, self._feat_dim)
+#         self.fc_4 = nn.Linear(self._feat_dim, self._feat_dim)
+#         self.fc_5 = nn.Linear(self._feat_dim + self._pos_dim, self._feat_dim)
+#         self.fc_6 = nn.Linear(self._feat_dim, self._feat_dim)
+#         self.fc_7 = nn.Linear(self._feat_dim, self._feat_dim)
+#         self.fc_8 = nn.Linear(self._feat_dim, self._feat_dim + density_dim)
+#         self.fc_9 = nn.Linear(self._feat_dim + self._view_dir_dim, self._feat_dim // 2)
+#         self.fc_out = nn.Linear(self._feat_dim // 2, rgb_dim)
+
+#         # activation layer
+#         self.relu_actvn = nn.ReLU()
+#         self.sigmoid_actvn = nn.Sigmoid()
+
+#     def forward(
+#         self,
+#         pos: torch.Tensor,
+#         view_dir: torch.Tensor,
+#     ) -> Tuple[torch.Tensor, torch.Tensor]:
+#         """
+#         Predicts color and density.
+
+#         Given sample point coordinates and view directions,
+#         predict the corresponding radiance (RGB) and density (sigma).
+
+#         Args:
+#             pos (torch.Tensor): Tensor of shape (N, self.pos_dim).
+#                 Coordinates of sample points along rays.
+#             view_dir (torch.Tensor): Tensor of shape (N, self.dir_dim).
+#                 View direction vectors.
+
+#         Returns:
+#             sigma (torch.Tensor): Tensor of shape (N, ).
+#                 Density at the given sample points.
+#             rgb (torch.Tensor): Tensor of shape (N, 3).
+#                 Radiance at the given sample points.
+#         """
+#         # check input tensors
+#         if (pos.ndim != 2) or (view_dir.ndim != 2):
+#             raise ValueError(f"Expected 2D tensors. Got {pos.ndim}, {view_dir.ndim}-D tensors.")
+#         if pos.shape[0] != view_dir.shape[0]:
+#             raise ValueError(
+#                 f"The number of samples must match. Got {pos.shape[0]} and {view_dir.shape[0]}."
+#             )
+#         if pos.shape[-1] != self._pos_dim:
+#             raise ValueError(f"Expected {self._pos_dim}-D position vector. Got {pos.shape[-1]}.")
+#         if view_dir.shape[-1] != self._view_dir_dim:
+#             raise ValueError(
+#                 f"Expected {self._view_dir_dim}-D view direction vector. Got {view_dir.shape[-1]}."
+#             )
+
+#         x = self.relu_actvn(self.fc_in(pos))
+#         x = self.relu_actvn(self.fc_1(x))
+#         x = self.relu_actvn(self.fc_2(x))
+#         x = self.relu_actvn(self.fc_3(x))
+#         x = self.relu_actvn(self.fc_4(x))
+
+#         x = torch.cat([pos, x], dim=-1)
+
+#         x = self.relu_actvn(self.fc_5(x))
+#         x = self.relu_actvn(self.fc_6(x))
+#         x = self.relu_actvn(self.fc_7(x))
+#         x = self.fc_8(x)
+
+#         sigma = self.relu_actvn(x[:, 0])
+#         x = torch.cat([x[:, 1:], view_dir], dim=-1)
+
+#         x = self.relu_actvn(self.fc_9(x))
+#         rgb = self.sigmoid_actvn(self.fc_out(x))
+
+#         return sigma, rgb

torch_nerf/src/renderer/integrators/quadrature_integrator.py
@@ -45,8 +45,48 @@ class QuadratureIntegrator(IntegratorBase):
         # HINT: Look up the documentation of 'torch.cumsum'.
         sig_mul_del = sigma * delta
         opacity = 1 - torch.exp(-sig_mul_del)
-        transmittance = torch.cumsum(1 - opacity, dim=1)
+        #wrong ver
+        #transmittance = torch.cumsum(1 - opacity, dim=1)
+
+        #ver1
+        #transmittance = torch.exp(torch.cumsum(-sig_mul_del, dim=1))
+
+        #ver2
+        transmittance = torch.exp(torch.cat((torch.zeros((sig_mul_del.shape[0],1), device = sig_mul_del.device), torch.cumsum(-sig_mul_del[:,:-1], dim=1)), dim = 1))
+        
         weight_origin = (transmittance * opacity)
-        weight = weight_origin[:,:,None].repeat(1, 1, 3)
+        #weight = weight_origin[:,:,None].repeat(1, 1, 3)
+        weight = weight_origin.unsqueeze(dim=-1)
         C_r = torch.sum(radiance * weight, dim=1)
         return C_r, weight_origin
+
+
+
+        #solution code
+        # sigma_delta = sigma * delta
+
+        # # compute transmittance: T_{i}
+        # transmittance = torch.exp(
+        #     -torch.cumsum(
+        #         torch.cat(
+        #             [torch.zeros((sigma.shape[0], 1), device=sigma_delta.device), sigma_delta],
+        #             dim=-1,
+        #         ),
+        #         dim=-1,
+        #     )[..., :-1]
+        # )
+
+        # # compute alpha: (1 - exp (- sigma_{i} * delta_{i}))
+        # alpha = 1.0 - torch.exp(-sigma_delta)
+
+        # # compute weight - w_{i}
+        # w_i = transmittance * alpha
+
+        # # compute numerical integral to determine pixel colors
+        # # C = sum_{i=1}^{S} T_{i} * alpha_{i} * c_{i}
+        # rgb = torch.sum(
+        #     w_i.unsqueeze(-1) * radiance,
+        #     dim=1,
+        # )
+
+        # return rgb, w_i

torch_nerf/src/renderer/ray_samplers/stratified_sampler.py
@@ -67,15 +67,18 @@ class StratifiedSampler(RaySamplerBase):
         # HINT: Freely use the provided methods 'create_t_bins' and 'map_t_to_euclidean'
 
         num_ray = ray_bundle.origins.shape[0]
-        t_bins = self.create_t_bins(num_sample + 1, ray_bundle.origins.device)
+        t_samples = self.create_t_bins(num_sample + 1, ray_bundle.origins.device)[None, :-1].repeat(num_ray, 1)
         # Uniformly sample [num_ray num_sample].
-        uniform_samples:Float[torch.Tensor, "num_ray num_sample"] = torch.rand(num_ray, num_sample, device=ray_bundle.origins.device)
-        for j in range(uniform_samples.shape[1]):
-            uniform_samples[:, j] = self.map_t_to_euclidean(uniform_samples[:, j], t_bins[j].item(), t_bins[j+1].item())
+        partition = 1/(num_sample)
+        t_samples:Float[torch.Tensor, "num_ray num_sample"] = t_samples + partition * torch.rand(num_ray, num_sample, device=ray_bundle.origins.device)
+        # for j in range(uniform_samples.shape[1]):
+        #     uniform_samples[:, j] = self.map_t_to_euclidean(uniform_samples[:, j], t_bins[j].item(), t_bins[j+1].item())
         #Mapping the uniform_samples to the Euclidean space.
-        t_samples = uniform_samples
-        for i in range(len(t_samples)):
-            t_samples[i] = self.map_t_to_euclidean(t_samples[i], ray_bundle.nears[i].item(), ray_bundle.fars[i].item())
+        # t_samples = uniform_samples
+        # for i in range(len(t_samples)):
+        #     t_samples[i] = self.map_t_to_euclidean(t_samples[i], ray_bundle.nears[i].item(), ray_bundle.fars[i].item())
+        #
+        t_samples = t_samples * (ray_bundle.fars[:,None].repeat(1,t_samples.shape[1]) - ray_bundle.nears[:,None].repeat(1,t_samples.shape[1])) + ray_bundle.nears[:,None].repeat(1,t_samples.shape[1])
         
         return t_samples
 